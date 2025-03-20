fb98f7448fb2b1483fa7ceb3f3591276b9b7b2ae
Cuong <duccuongkun@gmail.com>
2025-03-20 21:16:34 +0700

    update

---
+++ shopbannhoabackend/src/main/java/com/example/shopbannhoabackend/ShopbannhoabackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopbannhoabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopbannhoabackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopbannhoabackendApplication.class, args);
+    }
+
+}
---
+++ shopbannhoabackend/src/test/java/com/example/shopbannhoabackend/ShopbannhoabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopbannhoabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopbannhoabackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
